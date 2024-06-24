b01fc9187374a180029600836868e33328a86fd7
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-24 18:20:22 +0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Nick Krasnitskiy on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
