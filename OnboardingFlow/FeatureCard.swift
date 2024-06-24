d9aac1bb37162635d57fd749c8681d0dc5865bc1
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-24 19:11:07 +0300

    Create the features page.

---
+++ OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,33 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by Nick Krasnitskiy on 24.06.2024.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+                .font(.largeTitle)
+                .frame(width: 50)
+                .padding(.trailing, 10)
+            
+            Text(description)
+            
+            Spacer()
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+}
