f576223445618d1de91a3550787f40ca1fe39c2f
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-24 18:24:59 +0300

    Create the Welcome Page.

---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Nick Krasnitskiy on 24.06.2024.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text("Welcome to MyApp")
+            .font(.title)
+            .fontWeight(.semibold)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
